7ccaf6f359ce80ebe86c87cc40696fde5da0ca17
Angela <angela.liu1399@gmail.com>
2019-08-18 16:59:31 -0700

    Finish Charm model

--- app/src/main/java/com/example/lockit/models/Charm.java
+++ app/src/main/java/com/example/lockit/models/Charm.java
@@ -2,14 +2,49 @@ package com.example.lockit.models;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @ParseClassName("Charm")
 public class Charm extends ParseObject {
 
-    public static final String KEY_USR = "user";
+    public static final String KEY_USER = "user";
     public static final String KEY_ORG_NAME = "orgName";
     public static final String KEY_USER_NAME = "username";
     public static final String KEY_PASSWORD = "password";
 
     public Charm() { }
+
+    // Getter methods
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public String getOrgName() {
+        return getString(KEY_ORG_NAME);
+    }
+
+    public String getUserName() {
+        return getString(KEY_USER_NAME);
+    }
+
+    public String getPassword() {
+        return getString(KEY_PASSWORD);
+    }
+
+    // Setter methods
+    public void setUser(ParseUser user) {
+        put(KEY_USER, user);
+    }
+
+    public void setOrgName(String orgName) {
+        put(KEY_ORG_NAME, orgName);
+    }
+
+    public void setUserName(String username) {
+        put(KEY_USER_NAME, username);
+    }
+
+    public void setPassword(String password) {
+        put(KEY_PASSWORD, password);
+    }
 }
